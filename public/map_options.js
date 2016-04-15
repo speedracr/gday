e0d3ca54947800f9553bb26cbc42035613742813
Jonas Knipper <jonas@robocomobo.com>
2016-04-15 17:34:34 +0200

    Update map options

---
+++ public/map_options.js
@@ -0,0 +1,15 @@
+(function(window, mapster){
+  mapster.MAP_OPTIONS = {
+    center: {
+      lat: 37.79,
+      lng: -122.43
+    },
+    zoom: 10,
+    disableDefaultUI: true,
+    scrollwheel: false
+  };
+
+
+
+
+}(window, window.Mapster || (window.Mapster = {})))
