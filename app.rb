6dc753b688a3cd1dece56662ecfa7a35011c6dda
Jonas Knipper <jonas@robocomobo.com>
2016-04-28 12:54:21 +0200

    auf Twitter-gem wechseln

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'dotenv'
-require 'instagram'
+require 'twitter'
 Dotenv.load
 
 enable :sessions
@@ -8,95 +8,28 @@ set :session_secret, 'mysupersecretsession'
 
 CALLBACK_URL = 'http://localhost:9393/oauth/callback'
 
-Instagram.configure do |config|
-  config.client_id = ENV['INSTAGRAM_ID']
-  config.client_secret = ENV['INSTAGRAM_SECRET']
+
+$client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_KEY']
+  config.consumer_secret     = ENV['TWITTER_SECRET']
+  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
 end
 
-get '/' do
- '<a href="/oauth/connect">Connect with Instagram</a>'
+def get_tweets_for_search_term(searchterm)
+  $client.search(searchterm).take(10)
 end
 
-get "/oauth/connect" do
-  redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+get '/images' do
+  erb :images
 end
 
-get "/oauth/callback" do
-  response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
-  session[:access_token] = response.access_token
-  redirect "/nav"
+get '/twitter' do
+  erb :twitter
 end
 
-get "/token" do
-  erb :token
+get '/twitterresults' do
+  @tweets = get_tweets_for_search_term(params[:searchterm])
+  erb :twitterresults
 end
 
-get "/nav" do
-  html =
-  """
-    <h1>Ruby Instagram Gem Sample Application</h1>
-    <ol>
-      <li><a href='/user_recent_media'>User Recent Media</a> Calls user_recent_media - Get a list of a user's most recent media</li>
-      <li><a href='/user_media_feed'>User Media Feed</a> Calls user_media_feed - Get the currently authenticated user's media feed uses pagination</li>
-      <li><a href='/location_recent_media'>Location Recent Media</a> Calls location_recent_media - Get a list of recent media at a given location, in this case, the Instagram office</li>
-      <li><a href='/media_search'>Media Search</a> Calls media_search - Get a list of media close to a given latitude and longitude</li>
-      <li><a href='/media_popular'>Popular Media</a> Calls media_popular - Get a list of the overall most popular media items</li>
-      <li><a href='/user_search'>User Search</a> Calls user_search - Search for users on instagram, by name or username</li>
-      <li><a href='/location_search'>Location Search</a> Calls location_search - Search for a location by lat/lng</li>
-      <li><a href='/location_search_4square'>Location Search - 4Square</a> Calls location_search - Search for a location by Fousquare ID (v2)</li>
-      <li><a href='/tags'>Tags</a>Search for tags, view tag info and get media by tag</li>
-      <li><a href='/limits'>View Rate Limit and Remaining API calls</a>View remaining and ratelimit info.</li>
-    </ol>
-  """
-  html
-end
-
-get "/feed" do
-  erb :feed
-end
-
-get "/map" do
-  erb :map
-end
-
-get "/user_media_feed" do
-  client = Instagram.client(:access_token => session[:access_token])
-  user = client.user
-  html = "<h1>#{user.username}'s media feed</h1>"
-
-  page_1 = client.user_media_feed(777)
-  page_2_max_id = page_1.pagination.next_max_id
-  page_2 = client.user_recent_media(777, :max_id => page_2_max_id ) unless page_2_max_id.nil?
-  html << "<h2>Page 1</h2><br/>"
-  for media_item in page_1
-    html << "<img src='#{media_item.images.thumbnail.url}'>"
-  end
-  html << "<h2>Page 2</h2><br/>"
-  for media_item in page_2
-    html << "<img src='#{media_item.images.thumbnail.url}'>"
-  end
-  html
-end
-
-get "/user_recent_media" do
-  client = Instagram.client(:access_token => session[:access_token])
-  user = client.user
-  html = "<h1>#{user.username}'s recent media</h1>"
-  for media_item in client.user_recent_media
-    html << "<div style='float:left;'><img src='#{media_item.images.thumbnail.url}'><br/> <a href='/media_like/#{media_item.id}'>Like</a>  <a href='/media_unlike/#{media_item.id}'>Un-Like</a>  <br/>LikesCount=#{media_item.likes[:count]}</div>"
-  end
-  html
-end
-
-get "/user_search" do
-  client = Instagram.client(:access_token => session[:access_token])
-  html = "<h1>Search for users on instagram, by name or usernames</h1>"
-  for user in client.user_search("instagram")
-    html << "<li> <img src='#{user.profile_picture}'> #{user.username} #{user.full_name}</li>"
-  end
-  html
-end
-
-get '/search' do
-  erb :search
-end
